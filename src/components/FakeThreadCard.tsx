9f0fe0e6eb64b9c6939e18d6f47394fdf2dce008
Kingsten100 <isak9910@gmail.com>
2026-09-20 09:15:15 +0200

    Updates

--- src/components/FakeThreadCard.tsx
+++ src/components/FakeThreadCard.tsx
@@ -13,12 +13,12 @@ const FakeThreadCard = () => {
         <div className='tag'><span>Katter</span></div>
       </div>
       <div className='thread-title'>
-        <h2>Ge mig tips på vad katten ska heta i mellannamn</h2>
+        <h2>* FAKE THREAD * Ge mig tips på vad katten ska heta i mellannamn</h2>
       </div>
       <div className='thread-body-text'>
 
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam exercitationem soluta natus eius culpa ipsam illo deserunt delectus laborum voluptas recusandae nihil ullam perferendis voluptatibus, dolorum sint! Reprehenderit non at maxime quaerat magni! Consectetur atque expedita enim porro aut qui maiores cupiditate, molestiae nam deleniti, temporibus ex? Nihil, iure doloremque!
+          * This thread will be removed once a real thread is created * Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam exercitationem soluta natus eius culpa ipsam illo deserunt delectus laborum voluptas recusandae nihil ullam perferendis voluptatibus, dolorum sint! Reprehenderit non at maxime quaerat magni! Consectetur atque expedita enim porro aut qui maiores cupiditate, molestiae nam deleniti, temporibus ex? Nihil, iure doloremque!
         </p>
       <div className='comments-count'>
         <div className='comments-info'>
